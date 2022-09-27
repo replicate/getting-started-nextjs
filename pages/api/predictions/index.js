5ad46d0f80418576505762307c5eea54c6f08364
LyghtCode <91494328+LyghtCode@users.noreply.github.com>
2022-09-27 00:52:46 -0600

    Update index.js
    Authorization was missing quotes. Tested multiple times. Thank You!!!

--- pages/api/predictions/index.js
+++ pages/api/predictions/index.js
@@ -2,7 +2,7 @@ export default async function handler(req, res) {
   const response = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {
-      Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
+      "Authorization": `Token ${process.env.REPLICATE_API_TOKEN}`,
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
