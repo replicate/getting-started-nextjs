5d4f38194df9b69d707dba0d34b92854593c5dd8
Zeke Sikelianos <zeke@sikelianos.com>
2023-03-23 13:47:53 -0700

    use the replicate npm package

--- pages/api/predictions/[id].js
+++ pages/api/predictions/[id].js
@@ -1,20 +1,17 @@
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
 export default async function handler(req, res) {
-  const response = await fetch(
-    "https://api.replicate.com/v1/predictions/" + req.query.id,
-    {
-      headers: {
-        Authorization: `Token ${process.env.REPLICATE_API_TOKEN}`,
-        "Content-Type": "application/json",
-      },
-    }
-  );
-  if (response.status !== 200) {
-    let error = await response.json();
+  const prediction = await replicate.predictions.get(req.query.id);
+
+  if (prediction?.error) {
     res.statusCode = 500;
-    res.end(JSON.stringify({ detail: error.detail }));
+    res.end(JSON.stringify({ detail: prediction.error }));
     return;
   }
 
-  const prediction = await response.json();
   res.end(JSON.stringify(prediction));
 }
