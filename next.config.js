cbdc521b8cac10e336bf3ff973da9e8249cd25d8
Aron Carroll <aron@replicate.com>
2023-09-19 16:38:01 +0100

    Update remotePatterns in next.config.js

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "replicate.delivery",
       },
+      {
+        protocol: "https",
+        hostname: "*.replicate.delivery",
+      },
     ],
   },
 };
