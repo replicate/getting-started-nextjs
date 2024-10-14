55479fd756dfe1658c924f68b3ac98debca7c691
BinaryLady <sonia@thetechmargin.com>
2024-10-14 07:17:22 -0400

    new

--- app/page.js
+++ app/page.js
@@ -51,7 +51,7 @@ export default function Home() {
   return (
     <div className="container max-w-2xl mx-auto p-5">
       <h1 className="py-6 text-center font-bold text-2xl">
-        Dream something with{" "}
+        Imagine with AI {" "}//here
         <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
           SDXL
         </a>
