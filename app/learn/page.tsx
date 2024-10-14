20e3c19c29dc917a36ef85e3618bc20d4786814f
BinaryLady <sonia@thetechmargin.com>
2024-10-14 19:45:05 -0400

    "use client"

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
