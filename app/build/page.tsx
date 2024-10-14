737fa3a5a32b365f3221f57ec4b0cdf384563a81
BinaryLady <sonia@thetechmargin.com>
2024-10-14 19:30:24 -0400

    'use client';

--- app/build/page.tsx
+++ app/build/page.tsx
@@ -1,5 +1,4 @@
-'use client';
-
+import { useState, useEffect } from 'react'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
