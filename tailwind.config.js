ec065fa0feaf5a8c6e28d5ee5b432f622cced1ef
BinaryLady <sonia@thetechmargin.com>
2024-10-14 11:55:58 -0400

    all

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,13 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: ["class", 'class'],
+	module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
     './src/**/*.{ts,tsx}',
+		"./components/ui/**/*.{js,ts,jsx,tsx,mdx}",
 	],
   theme: {
   	container: {
