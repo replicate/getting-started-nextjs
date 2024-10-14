075680a54d30892f83445e0356d2f3984c28e406
BinaryLady <sonia@thetechmargin.com>
2024-10-14 08:56:42 -0400

    tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
     './src/**/*.{ts,tsx}',
 	],
   theme: {
