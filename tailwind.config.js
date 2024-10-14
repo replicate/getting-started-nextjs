1d5a44f60a4ba8267f70f85a26078802e6b6f6b6
BinaryLady <sonia@thetechmargin.com>
2024-10-14 09:05:04 -0400

    tw

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
