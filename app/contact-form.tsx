edca586c4a6b82b8ebc380980eb981fb64fdaba4
BinaryLady <sonia@thetechmargin.com>
2024-10-14 10:58:16 -0400

    all ui

--- app/contact-form.tsx
+++ app/contact-form.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/header"
+import Header from "@/components/ui/header"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
@@ -15,7 +15,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
-import { toast } from "@/components/ui/use-toast"
+import { toast } from "@/components/ui/toast"
 
 const formSchema = z.object({
   name: z.string().min(2, {
