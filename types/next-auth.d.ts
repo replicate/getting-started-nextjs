d5520265f2106cb4d0da7569ad7bd638c3be5b09
BinaryLady <sonia@thetechmargin.com>
2024-10-15 21:22:26 -0400

    user

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,12 +3,17 @@ import NextAuth, { DefaultSession } from "next-auth"
 declare module "next-auth" {
   interface Session {
     user: {
-      id: string
-      accessToken?: string
-      refreshToken?: string
-      username?: string
-    } & DefaultSession["user"]
+      id: string;
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      accessToken?: string;
+      refreshToken?: string;
+      username?: string;
+    } & DefaultSession["user"];
   }
+
+
 
   interface User {
     id: string
