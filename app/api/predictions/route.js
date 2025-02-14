0c902f752064fc212d5032380de7e9facc2686cf
Zeke Sikelianos <zeke@sikelianos.com>
2025-02-14 08:06:12 -0800

    use black-forest-labs/flux-schnell

--- app/api/predictions/route.js
+++ app/api/predictions/route.js
@@ -27,7 +27,7 @@ export async function POST(request) {
   const { prompt } = await request.json();
 
   const options = {
-    version: '8beff3369e81422112d93b89ca01426147de542cd4684c244b673b105188fe5f',
+    model: 'black-forest-labs/flux-schnell',
     input: { prompt }
   }
 
--- app/page.js
+++ app/page.js
@@ -52,8 +52,8 @@ export default function Home() {
     <div className="container max-w-2xl mx-auto p-5">
       <h1 className="py-6 text-center font-bold text-2xl">
         Dream something with{" "}
-        <a href="https://replicate.com/stability-ai/sdxl?utm_source=project&utm_project=getting-started">
-          SDXL
+        <a href="https://replicate.com/black-forest-labs/flux-schnell?utm_source=project&utm_project=getting-started">
+          Flux Schnell
         </a>
       </h1>
 
